c245ac4259d88c6c3d353d48adbbcb92f0a8663f
LLapuras <chenwanwan13@gmail.com>
2019-12-09 21:03:55 +0000

    UPDATE

---
+++ CSC8503/CSC8503Common/GameObject.h
@@ -0,0 +1,105 @@
+#pragma once
+#include "Transform.h"
+#include "CollisionVolume.h"
+
+#include "PhysicsObject.h"
+#include "RenderObject.h"
+#include "NetworkObject.h"
+
+#include <vector>
+
+using std::vector;
+
+namespace NCL {
+	namespace CSC8503 {
+		class NetworkObject;
+
+		class GameObject	{
+		public:
+			GameObject(string name = "");
+			~GameObject();
+
+			void SetBoundingVolume(CollisionVolume* vol) {
+				boundingVolume = vol;
+			}
+
+			const CollisionVolume* GetBoundingVolume() const {
+				return boundingVolume;
+			}
+
+			bool IsActive() const {
+				return isActive;
+			}
+
+			const Transform& GetConstTransform() const {
+				return transform;
+			}
+
+			Transform& GetTransform() {
+				return transform;
+			}
+
+			RenderObject* GetRenderObject() const {
+				return renderObject;
+			}
+
+			PhysicsObject* GetPhysicsObject() const {
+				return physicsObject;
+			}
+
+			NetworkObject* GetNetworkObject() const {
+				return networkObject;
+			}
+
+			void SetRenderObject(RenderObject* newObject) {
+				renderObject = newObject;
+			}
+
+			void SetPhysicsObject(PhysicsObject* newObject) {
+				physicsObject = newObject;
+			}
+
+			const string& GetName() const {
+				return name;
+			}
+
+			const string& GetLayer() const {
+				return layer;
+			}
+
+			void SetLayer(string layername) {
+				layer = layername;
+			}
+
+
+			virtual void OnCollisionBegin(GameObject* otherObject) {
+				//std::cout << "OnCollisionBegin event occured!\n";
+			}
+
+			virtual void OnCollisionEnd(GameObject* otherObject) {
+				//std::cout << "OnCollisionEnd event occured!\n";
+			}
+
+			bool GetBroadphaseAABB(Vector3&outsize) const;
+
+			void UpdateBroadphaseAABB();
+
+		protected:
+			Transform			transform;
+
+			CollisionVolume*	boundingVolume;
+			PhysicsObject*		physicsObject;
+			RenderObject*		renderObject;
+			NetworkObject*		networkObject;
+
+			bool	isActive;
+			string	name;
+
+			//layer-based collision
+			string layer;
+
+			Vector3 broadphaseAABB;
+		};
+	}
+}
+
